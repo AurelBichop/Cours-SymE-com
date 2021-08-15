b151a7bc203d6329a5bdfc9d22171e20ec4b2e78
aurelBichop <linux.aurelien@gmail.com>
2021-08-15 15:29:17 +0200

    ajout de formulaires

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Form\CategoryType;
+use App\Form\ProductType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/admin/category/create', name: 'category_create')]
+    public function new(Request $request, EntityManagerInterface $entityManager,SluggerInterface $slugger):Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class,$category);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $category->setSlug(strtolower($slugger->slug($category->getName())));
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('category_edit',['id'=>$category->getId()]);
+        }
+
+        return $this->render('category/create.html.twig',[
+            "form"=>$form->createView()
+        ]);
+    }
+
+    #[Route('/admin/category/edit/{id}', name: 'category_edit')]
+    public function edit(Category $category,Request $request, EntityManagerInterface $entityManager,SluggerInterface $slugger):Response
+    {
+        $form = $this->createForm(CategoryType::class,$category);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $category->setSlug(strtolower($slugger->slug($category->getName())));
+            $entityManager->flush();
+
+            return $this->redirectToRoute('category_edit',['id'=>$category->getId()]);
+        }
+
+        return $this->render('category/edit.html.twig',[
+            "form"=>$form->createView(),
+            "category"=>$category
+        ]);
+    }
+}
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,14 +3,59 @@
 namespace App\Controller;
 
 
+use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProductController extends AbstractController
 {
+    #[Route('/admin/product/create', name: 'product_create')]
+    public function new(Request $request, EntityManagerInterface $entityManager,SluggerInterface $slugger):Response
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class,$product);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $product->setSlug($slugger->slug($product->getName()));
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('product_edit',['id'=>$product->getId()]);
+        }
+
+        return $this->render('product/create.html.twig',[
+            "form"=>$form->createView()
+        ]);
+    }
+
+    #[Route('/admin/product/edit/{id}', name: 'product_edit')]
+    public function edit(Product $product,Request $request,SluggerInterface $slugger,EntityManagerInterface $entityManager):Response
+    {
+        $form = $this->createForm(ProductType::class,$product);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $product->setSlug($slugger->slug($product->getName()));
+            $entityManager->flush();
+
+            return $this->redirectToRoute('product_edit',['id'=>$product->getId()]);
+        }
+
+        return $this->render('product/edit.html.twig',[
+            "form"=>$form->createView(),
+            "product"=>$product
+        ]);
+    }
+
     #[Route('/{slug}', name: 'product_category')]
     public function category(string $slug, CategoryRepository $productRepository): Response
     {
